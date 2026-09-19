64e73ac88158372fa0ad86774e19f35eee46a7c2
frytaz <frytaz@gmail.com>
2026-09-20 00:08:44 +0200

    test(las): add visual loading tests for LAS/LAZ 1.4 and 1.5

--- tests/snapshots-loading-models.spec.js
+++ tests/snapshots-loading-models.spec.js
@@ -5,4 +5,7 @@ test.describe("Model loading visual test", async () => {
     testWithTimeout("loading_gltf_duplex");
     testWithTimeout("loading_gltf_duck_quantized");
     testWithTimeout("loading_laz_autzen");
+    testWithTimeout("loading_laz_1_4_copc");
+    testWithTimeout("loading_laz_1_5");
+    testWithTimeout("loading_las_1_5");
 });
